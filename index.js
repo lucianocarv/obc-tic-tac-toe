999b8db1e2b7ac35faca9cc81e163e4bedfb60f5
Luciano Carvalho <lucianocarv13@gmail.com>
2022-09-18 21:42:53 -0300

    variables and functions adjusted

--- index.js
+++ index.js
@@ -1,242 +1,240 @@
-let names = false;
-let player1;
-let player2;
+const initializeGameButton = document.querySelector("input[name='start']");
+initializeGameButton.addEventListener("click", preLoadGame);
 
-const initialScreen = document.querySelector(".initial-form");
-const p1 = document.querySelector("input[name='player1']");
-const p2 = document.querySelector("input[name='player2']");
-const textInputs = document.querySelectorAll(".player");
-const btStart = document.querySelector("input[name='start']");
-const main = document.querySelector("main");
+const HTMLMain = document.querySelector("main");
+const HTMLInitialScreenValid = document.querySelector(".initial-form");
+const HTMLPlayer1 = document.querySelector("input[name='player1']");
+const HTMLPlayer2 = document.querySelector("input[name='player2']");
+let player1, player2;
+HTMLPlayer1.focus();
 
-p1.focus();
+function preLoadGame(ev) {
+  if (ev.type == "click" || ev.key == "Enter") {
+    player1 = HTMLPlayer1.value;
+    player2 = HTMLPlayer2.value;
+    HTMLNameOfFirstPlayer.innerText = player1;
+    HTMLNameOfSecondPlayer.innerText = player2;
+    HTMLInitialScreenValid.setAttribute("id", "initial-close");
+    HTMLMain.classList.remove("main-op");
+  }
+}
 
-const user1 = document.getElementById("name-of-player-1");
-const user2 = document.getElementById("name-of-player-2");
-
-textInputs.forEach((input) => {
+const HTMLInputsPlayers = document.querySelectorAll(".player");
+HTMLInputsPlayers.forEach((input) => {
   input.addEventListener("input", function () {
-    let leng1 = Array.from(p1.value).length;
-    let leng2 = Array.from(p2.value).length;
-    if (leng1 > 1 && leng2 > 1) {
-      btStart.disabled = false;
-      window.addEventListener("keydown", loadGame);
+    let lenghtNamePlayer1 = Array.from(HTMLPlayer1.value).length;
+    let lenghtNamePlayer2 = Array.from(HTMLPlayer2.value).length;
+    if (lenghtNamePlayer1 > 1 && lenghtNamePlayer2 > 1) {
+      initializeGameButton.disabled = false;
+      window.addEventListener("keydown", preLoadGame);
     } else {
-      btStart.disabled = true;
+      initializeGameButton.disabled = true;
     }
   });
 });
-btStart.addEventListener("click", loadGame);
 
-function loadGame(ev) {
-  if (ev.type == "click" || ev.key == "Enter") {
-    player1 = p1.value;
-    player2 = p2.value;
-    user1.innerText = player1;
-    user2.innerText = player2;
-    initialScreen.setAttribute("id", "initial-close");
-    main.classList.remove("main-op");
-  }
-}
+const HTMLNameOfFirstPlayer = document.getElementById("name-of-player-1");
+const HTMLNameOfSecondPlayer = document.getElementById("name-of-player-2");
 
-const divResult = document.getElementById("results-game");
-const areaGame = document.querySelector(".area-game");
+const HTMLResultView = document.getElementById("results-game");
+const HTMLGameGridArea = document.querySelector(".area-game");
 
-const counterWinsPlayer1 = document.getElementById("counter-player-1");
-const counterWinsPlayer2 = document.getElementById("counter-player-2");
+const HTMLFirstPlayerWinsCount = document.getElementById("counter-player-1");
+const HTMLSecondPlayerWinsCount = document.getElementById("counter-player-2");
 
-const markers = document.querySelectorAll(".marker");
-markers.forEach((marker) => {
-  marker.addEventListener("click", mark);
+const HTMLPlayAgain = document.getElementById("refresh");
+const HTMLBoxesToBeMarked = document.querySelectorAll(".marker");
+
+HTMLBoxesToBeMarked.forEach((marker) => {
+  marker.addEventListener("click", handleBoxMarked);
 });
 
-const replayBtn = document.getElementById("refresh");
-replayBtn.addEventListener("click", function () {
-  markers.forEach((marker) => {
-    marker.addEventListener("click", mark);
+HTMLPlayAgain.addEventListener("click", function () {
+  HTMLBoxesToBeMarked.forEach((marker) => {
+    marker.addEventListener("click", handleBoxMarked);
     marker.innerHTML = "";
     marker.classList.remove("winner");
     marker.classList.remove("tie-rotate");
-    replayBtn.disabled = true;
+    HTMLPlayAgain.disabled = true;
   });
-  divResult.classList.remove("show-results");
-  areaGame.classList.remove("tie");
+  HTMLResultView.classList.remove("show-results");
+  HTMLGameGridArea.classList.remove("tie");
 
-  areaGame.classList.add("area-game-effect");
+  HTMLGameGridArea.classList.add("area-game-effect");
 });
 
-function mark(ev) {
-  areaGame.classList.remove("area-game-effect");
-  ev.target.removeEventListener("click", mark);
-  const icon = document.createElement("span");
-  icon.classList.add("material-symbols-outlined", "icon-size");
-  if (user1.classList.contains("current-player")) {
-    icon.innerText = "close";
-  } else if (user2.classList.contains("current-player")) {
-    icon.innerText = "circle";
+function handleBoxMarked(ev) {
+  HTMLGameGridArea.classList.remove("area-game-effect");
+  ev.target.removeEventListener("click", handleBoxMarked);
+  const HTMLIconGame = document.createElement("span");
+  HTMLIconGame.classList.add("material-symbols-outlined", "icon-size");
+  if (HTMLNameOfFirstPlayer.classList.contains("current-player")) {
+    HTMLIconGame.innerText = "close";
+  } else if (HTMLNameOfSecondPlayer.classList.contains("current-player")) {
+    HTMLIconGame.innerText = "circle";
   }
 
-  ev.target.appendChild(icon);
-  verify(ev.target);
-  user1.classList.toggle("current-player");
-  user2.classList.toggle("current-player");
+  ev.target.appendChild(HTMLIconGame);
+  checkElementChildToPushArray(ev.target);
+  HTMLNameOfFirstPlayer.classList.toggle("current-player");
+  HTMLNameOfSecondPlayer.classList.toggle("current-player");
   ev.target.firstChild.classList.add("marked");
 }
 
-let results = [
+let gameVirtualMatrix = [
   [false, true, false],
   [false, true, true],
   [true, false, false],
 ];
 
-function verify(element) {
+function checkElementChildToPushArray(element) {
   const id = element.id;
   if (id == "mark-1") {
-    results[0][0] = element.firstChild.innerText;
+    gameVirtualMatrix[0][0] = element.firstChild.innerText;
   } else if (id == "mark-2") {
-    results[0][1] = element.firstChild.innerText;
+    gameVirtualMatrix[0][1] = element.firstChild.innerText;
   } else if (id == "mark-3") {
-    results[0][2] = element.firstChild.innerText;
+    gameVirtualMatrix[0][2] = element.firstChild.innerText;
   } else if (id == "mark-4") {
-    results[1][0] = element.firstChild.innerText;
+    gameVirtualMatrix[1][0] = element.firstChild.innerText;
   } else if (id == "mark-5") {
-    results[1][1] = element.firstChild.innerText;
+    gameVirtualMatrix[1][1] = element.firstChild.innerText;
   } else if (id == "mark-6") {
-    results[1][2] = element.firstChild.innerText;
+    gameVirtualMatrix[1][2] = element.firstChild.innerText;
   } else if (id == "mark-7") {
-    results[2][0] = element.firstChild.innerText;
+    gameVirtualMatrix[2][0] = element.firstChild.innerText;
   } else if (id == "mark-8") {
-    results[2][1] = element.firstChild.innerText;
+    gameVirtualMatrix[2][1] = element.firstChild.innerText;
   } else if (id == "mark-9") {
-    results[2][2] = element.firstChild.innerText;
+    gameVirtualMatrix[2][2] = element.firstChild.innerText;
   }
-  verifyArr(results);
+  vMatrixCheck(gameVirtualMatrix);
 }
 
-let countForTie = 0;
-function verifyArr(results) {
-  if (results[0][0] == results[0][1] && results[0][0] == results[0][2]) {
-    markers[0].classList.add("winner");
-    markers[1].classList.add("winner");
-    markers[2].classList.add("winner");
-    verifyPlayer(results[0][0]);
-    breakGame();
-    showResult(results[0][0]);
-    countForTie = 0;
-  } else if (results[1][0] == results[1][1] && results[1][0] == results[1][2]) {
-    markers[3].classList.add("winner");
-    markers[4].classList.add("winner");
-    markers[5].classList.add("winner");
-    verifyPlayer(results[1][0]);
-    breakGame();
-    showResult(results[1][0]);
-    countForTie = 0;
-  } else if (results[2][0] == results[2][1] && results[2][0] == results[2][2]) {
-    markers[6].classList.add("winner");
-    markers[7].classList.add("winner");
-    markers[8].classList.add("winner");
-    verifyPlayer(results[2][0]);
-    breakGame();
-    showResult(results[2][0]);
-    countForTie = 0;
-  } else if (results[0][0] == results[1][0] && results[0][0] == results[2][0]) {
-    markers[0].classList.add("winner");
-    markers[3].classList.add("winner");
-    markers[6].classList.add("winner");
-    verifyPlayer(results[0][0]);
-    breakGame();
-    showResult(results[0][0]);
-    countForTie = 0;
-  } else if (results[0][1] == results[1][1] && results[0][1] == results[2][1]) {
-    markers[1].classList.add("winner");
-    markers[4].classList.add("winner");
-    markers[7].classList.add("winner");
-    verifyPlayer(results[0][1]);
-    breakGame();
-    showResult(results[0][1]);
-    countForTie = 0;
-  } else if (results[0][2] == results[1][2] && results[0][2] == results[2][2]) {
-    markers[2].classList.add("winner");
-    markers[5].classList.add("winner");
-    markers[8].classList.add("winner");
-    verifyPlayer(results[0][2]);
-    breakGame();
-    showResult(results[0][2]);
-    countForTie = 0;
-  } else if (results[0][0] == results[1][1] && results[0][0] == results[2][2]) {
-    markers[0].classList.add("winner");
-    markers[4].classList.add("winner");
-    markers[8].classList.add("winner");
-    verifyPlayer(results[0][0]);
-    breakGame();
-    showResult(results[0][0]);
-    countForTie = 0;
-  } else if (results[0][2] == results[1][1] && results[0][2] == results[2][0]) {
-    markers[2].classList.add("winner");
-    markers[4].classList.add("winner");
-    markers[6].classList.add("winner");
-    verifyPlayer(results[0][2]);
-    breakGame();
-    showResult(results[0][2]);
-    countForTie = 0;
+let countToTie = 0;
+function vMatrixCheck(vMatrix) {
+  if (vMatrix[0][0] == vMatrix[0][1] && vMatrix[0][0] == vMatrix[0][2]) {
+    HTMLBoxesToBeMarked[0].classList.add("winner");
+    HTMLBoxesToBeMarked[1].classList.add("winner");
+    HTMLBoxesToBeMarked[2].classList.add("winner");
+    checkWhichPlayer(vMatrix[0][0]);
+    stopGameAndFreezePlays();
+    showResultInPage(vMatrix[0][0]);
+    countToTie = 0;
+  } else if (vMatrix[1][0] == vMatrix[1][1] && vMatrix[1][0] == vMatrix[1][2]) {
+    HTMLBoxesToBeMarked[3].classList.add("winner");
+    HTMLBoxesToBeMarked[4].classList.add("winner");
+    HTMLBoxesToBeMarked[5].classList.add("winner");
+    checkWhichPlayer(vMatrix[1][0]);
+    stopGameAndFreezePlays();
+    showResultInPage(vMatrix[1][0]);
+    countToTie = 0;
+  } else if (vMatrix[2][0] == vMatrix[2][1] && vMatrix[2][0] == vMatrix[2][2]) {
+    HTMLBoxesToBeMarked[6].classList.add("winner");
+    HTMLBoxesToBeMarked[7].classList.add("winner");
+    HTMLBoxesToBeMarked[8].classList.add("winner");
+    checkWhichPlayer(vMatrix[2][0]);
+    stopGameAndFreezePlays();
+    showResultInPage(vMatrix[2][0]);
+    countToTie = 0;
+  } else if (vMatrix[0][0] == vMatrix[1][0] && vMatrix[0][0] == vMatrix[2][0]) {
+    HTMLBoxesToBeMarked[0].classList.add("winner");
+    HTMLBoxesToBeMarked[3].classList.add("winner");
+    HTMLBoxesToBeMarked[6].classList.add("winner");
+    checkWhichPlayer(vMatrix[0][0]);
+    stopGameAndFreezePlays();
+    showResultInPage(vMatrix[0][0]);
+    countToTie = 0;
+  } else if (vMatrix[0][1] == vMatrix[1][1] && vMatrix[0][1] == vMatrix[2][1]) {
+    HTMLBoxesToBeMarked[1].classList.add("winner");
+    HTMLBoxesToBeMarked[4].classList.add("winner");
+    HTMLBoxesToBeMarked[7].classList.add("winner");
+    checkWhichPlayer(vMatrix[0][1]);
+    stopGameAndFreezePlays();
+    showResultInPage(vMatrix[0][1]);
+    countToTie = 0;
+  } else if (vMatrix[0][2] == vMatrix[1][2] && vMatrix[0][2] == vMatrix[2][2]) {
+    HTMLBoxesToBeMarked[2].classList.add("winner");
+    HTMLBoxesToBeMarked[5].classList.add("winner");
+    HTMLBoxesToBeMarked[8].classList.add("winner");
+    checkWhichPlayer(vMatrix[0][2]);
+    stopGameAndFreezePlays();
+    showResultInPage(vMatrix[0][2]);
+    countToTie = 0;
+  } else if (vMatrix[0][0] == vMatrix[1][1] && vMatrix[0][0] == vMatrix[2][2]) {
+    HTMLBoxesToBeMarked[0].classList.add("winner");
+    HTMLBoxesToBeMarked[4].classList.add("winner");
+    HTMLBoxesToBeMarked[8].classList.add("winner");
+    checkWhichPlayer(vMatrix[0][0]);
+    stopGameAndFreezePlays();
+    showResultInPage(vMatrix[0][0]);
+    countToTie = 0;
+  } else if (vMatrix[0][2] == vMatrix[1][1] && vMatrix[0][2] == vMatrix[2][0]) {
+    HTMLBoxesToBeMarked[2].classList.add("winner");
+    HTMLBoxesToBeMarked[4].classList.add("winner");
+    HTMLBoxesToBeMarked[6].classList.add("winner");
+    checkWhichPlayer(vMatrix[0][2]);
+    stopGameAndFreezePlays();
+    showResultInPage(vMatrix[0][2]);
+    countToTie = 0;
   } else {
-    countForTie += 1;
-    if (countForTie == 9) {
-      tie();
-      countForTie = 0;
+    countToTie += 1;
+    if (countToTie == 9) {
+      chekGameTie();
+      countToTie = 0;
     }
   }
 }
 
-function tie() {
-  breakGame();
-  markers.forEach((marker) => {
+function chekGameTie() {
+  stopGameAndFreezePlays();
+  HTMLBoxesToBeMarked.forEach((marker) => {
     marker.classList.add("tie-rotate");
   });
-  areaGame.classList.add("tie");
-  showResult("empate");
+  HTMLGameGridArea.classList.add("tie");
+  showResultInPage("empate");
 }
 
-function breakGame() {
-  replayBtn.disabled = false;
-  markers.forEach((marker) => {
-    marker.removeEventListener("click", mark);
+function stopGameAndFreezePlays() {
+  HTMLPlayAgain.disabled = false;
+  HTMLBoxesToBeMarked.forEach((marker) => {
+    marker.removeEventListener("click", handleBoxMarked);
   });
-  results = [
+  gameVirtualMatrix = [
     [false, true, false],
     [false, true, true],
     [true, false, false],
   ];
 }
-function verifyPlayer(value) {
-  replayBtn.disabled = false;
+function checkWhichPlayer(value) {
+  HTMLPlayAgain.disabled = false;
   if (value === "close") {
     const player = player1;
-    let wins = Number(counterWinsPlayer1.innerText);
+    let wins = Number(HTMLFirstPlayerWinsCount.innerText);
     wins += 1;
-    counterWinsPlayer1.innerText = wins;
+    HTMLFirstPlayerWinsCount.innerText = wins;
     return player1;
   }
   if (value === "circle") {
     const player = player2;
-    let wins = Number(counterWinsPlayer2.innerText);
+    let wins = Number(HTMLSecondPlayerWinsCount.innerText);
     wins += 1;
-    counterWinsPlayer2.innerText = wins;
+    HTMLSecondPlayerWinsCount.innerText = wins;
     return player2;
   } else if (value === 3) {
-    showResult("empate");
+    showResultInPage("empate");
   }
 }
 
-function showResult(name) {
+function showResultInPage(name) {
   if (name == "circle") {
     document.querySelector("#results-game h2").innerText = player2 + " venceu!";
-    divResult.classList.add("show-results");
+    HTMLResultView.classList.add("show-results");
   } else if (name == "close") {
     document.querySelector("#results-game h2").innerText = player1 + " venceu!";
-    divResult.classList.add("show-results");
+    HTMLResultView.classList.add("show-results");
   } else if ((name = "empate")) {
     document.querySelector("#results-game h2").innerText = "Empate";
-    divResult.classList.add("show-results");
+    HTMLResultView.classList.add("show-results");
   }
 }
